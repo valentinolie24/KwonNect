5641ec852038426c7b0f3fa136cc09f153935cff
valentinolie24 <72203537+valentinolie24@users.noreply.github.com>
2023-06-13 23:15:39 +0700

    Splash dan item list

---
+++ app/src/main/java/com/valen/kwonnect/Splash.java
@@ -0,0 +1,28 @@
+package com.valen.kwonnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
