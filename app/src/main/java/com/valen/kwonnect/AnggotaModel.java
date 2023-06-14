66b421998b703fc765aeb49aa354185a68079ff4
valentinolie24 <72203537+valentinolie24@users.noreply.github.com>
2023-06-14 23:28:33 +0700

    Create AnggotaModel.java

---
+++ app/src/main/java/com/valen/kwonnect/AnggotaModel.java
@@ -0,0 +1,94 @@
+package com.valen.kwonnect;
+
+public class AnggotaModel {
+    private String email;
+    private String nama;
+    private String npm;
+    private String prodi;
+    private String foto;
+    private String tempat;
+    private String tanggal;
+    private String berat;
+    private String tinggi;
+    private String nowa;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getTempat() {
+        return tempat;
+    }
+
+    public void setTempat(String tempat) {
+        this.tempat = tempat;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getBerat() {
+        return berat;
+    }
+
+    public void setBerat(String berat) {
+        this.berat = berat;
+    }
+
+    public String getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(String tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    public String getNowa() {
+        return nowa;
+    }
+
+    public void setNowa(String nowa) {
+        this.nowa = nowa;
+    }
+}
