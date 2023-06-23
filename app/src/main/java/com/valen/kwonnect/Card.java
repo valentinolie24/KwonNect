f9504a420f739e34050f4966532a7b89f5b55f84
valentinolie24 <72203537+valentinolie24@users.noreply.github.com>
2023-06-23 16:40:49 +0700

    PROJECT DONEEEEEEEEE

--- app/src/main/java/com/valen/kwonnect/Card.java
+++ app/src/main/java/com/valen/kwonnect/Card.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,21 +17,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Card extends RecyclerView.Adapter<Card.ClassViewHolder> {
     private ArrayList<AnggotaModel> dataAnggota;
     private Context ctx;
     private OnItemLongClickListener onItemLongClickListener;
+    private Card cardAdapter;
 
     public Card(ArrayList<AnggotaModel> dataAnggota, Context ctx) {
         this.dataAnggota = dataAnggota;
         this.ctx = ctx;
+        this.cardAdapter = this;
     }
 
-    public Card() {
-        this.dataAnggota = dataAnggota;
-        this.ctx = ctx;
-    }
 
     @NonNull
     @Override
@@ -84,14 +88,111 @@ public class Card extends RecyclerView.Adapter<Card.ClassViewHolder> {
         });
 
         holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-
-            @Override
             public boolean onLongClick(View v) {
+                AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                pesan.setTitle("Perhatian");
+                pesan.setMessage("Anda memilih anggota dengan nama " + anggota.getNama());
+                pesan.setCancelable(true);
+
+                pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteAnggota(anggota.getId());
+                    }
+                });
+
+                pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent kirim = new Intent(ctx, UpdateAnggota.class);
+                        kirim.putExtra("xId", anggota.getId());
+                        kirim.putExtra("xFoto", anggota.getFoto());
+                        kirim.putExtra("xEmail", anggota.getEmail());
+                        kirim.putExtra("xNama", anggota.getNama());
+                        kirim.putExtra("xNpm", anggota.getNpm());
+                        kirim.putExtra("xProdi", anggota.getProdi());
+                        kirim.putExtra("xSabuk", anggota.getSabuk());
+                        kirim.putExtra("xTempat", anggota.getTempat_lahir());
+                        kirim.putExtra("xTanggal", anggota.getTanggal_lahir());
+                        kirim.putExtra("xTinggi", anggota.getTinggi_badan());
+                        kirim.putExtra("xBerat", anggota.getBerat_badan());
+                        kirim.putExtra("xNoWa", anggota.getNomor_whatsapp());
+
+                        ctx.startActivity(kirim);
+                    }
+                });
+
+                pesan.show();
                 return false;
             }
         });
     }
 
+    private void deleteAnggota(String id) {
+        APIService api = Utilities.getRetrofit().create(APIService.class);
+        Call<ValueNoData> call = api.deleteAnggota(id);
+        call.enqueue(new Callback<ValueNoData>() {
+            @Override
+            public void onResponse(Call<ValueNoData> call, Response<ValueNoData> response) {
+                if (response.code() == 200){
+                    int success = response.body().getSuccess();
+                    String message = response.body().getMessage();
+
+                    if (success == 1){
+                        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+                        getAllAnggota();
+                    } else {
+                        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ctx, "Response " + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ValueNoData> call, Throwable t) {
+                Toast.makeText(ctx, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private int findPositionById(String id) {
+        for (int i = 0; i < dataAnggota.size(); i++) {
+            if (dataAnggota.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void getAllAnggota() {
+        APIService apiService = Utilities.getRetrofit().create(APIService.class);
+        Call<ValueData<List<AnggotaModel>>> call = apiService.getAnggota();
+        call.enqueue(new Callback<ValueData<List<AnggotaModel>>>() {
+            @Override
+            public void onResponse(Call<ValueData<List<AnggotaModel>>> call, Response<ValueData<List<AnggotaModel>>> response) {
+                if (response.isSuccessful()) {
+                    ValueData<List<AnggotaModel>> valueData = response.body();
+                    if (valueData != null && valueData.getSuccess() == 1) {
+                        dataAnggota.clear();
+                        dataAnggota.addAll(valueData.getData());
+                        cardAdapter.setData(dataAnggota); // Update data dalam adapter
+                        cardAdapter.notifyDataSetChanged();
+                        Toast.makeText(ctx, valueData.getMessage(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(ctx, "Failed to get data: " + valueData.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ctx, "Response failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ValueData<List<AnggotaModel>>> call, Throwable t) {
+                Toast.makeText(ctx, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     @Override
     public int getItemCount() {
         return dataAnggota.size();
--- app/src/main/java/com/valen/kwonnect/Grid.java
+++ app/src/main/java/com/valen/kwonnect/Grid.java
@@ -1,11 +1,14 @@
 package com.valen.kwonnect;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,19 +16,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Grid extends RecyclerView.Adapter<Grid.ClassViewHolder> {
     private ArrayList<AnggotaModel> dataAnggota;
+    private OnItemLongClickListener onItemLongClickListener;
     private Context ctx;
+    private Grid gridAdapter;
 
     public Grid(ArrayList<AnggotaModel> dataAnggota, Context ctx) {
         this.dataAnggota = dataAnggota;
         this.ctx = ctx;
-    }
-
-    public Grid() {
-        this.dataAnggota = dataAnggota;
-        this.ctx = ctx;
+        this.gridAdapter = this;
     }
 
     @NonNull
@@ -38,27 +44,24 @@ public class Grid extends RecyclerView.Adapter<Grid.ClassViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
         AnggotaModel anggota = dataAnggota.get(position);
-        Glide
-                .with(ctx)
+        Glide.with(ctx)
                 .load(anggota.getFoto())
                 .into(holder.ivGrid);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String xFoto, xEmail, xNama, xNpm, xProdi, xSabuk, xTempat, xTanggal, xTinggi, xBerat, xNoWa;
-
-                xFoto = anggota.getFoto();
-                xEmail = anggota.getEmail();
-                xNama = anggota.getNama();
-                xNpm = anggota.getNpm();
-                xProdi = anggota.getProdi();
-                xSabuk = anggota.getSabuk();
-                xTempat = anggota.getTempat_lahir();
-                xTanggal = anggota.getTanggal_lahir();
-                xTinggi = anggota.getTinggi_badan();
-                xBerat = anggota.getBerat_badan();
-                xNoWa = anggota.getNomor_whatsapp();
+                String xFoto = anggota.getFoto();
+                String xEmail = anggota.getEmail();
+                String xNama = anggota.getNama();
+                String xNpm = anggota.getNpm();
+                String xProdi = anggota.getProdi();
+                String xSabuk = anggota.getSabuk();
+                String xTempat = anggota.getTempat_lahir();
+                String xTanggal = anggota.getTanggal_lahir();
+                String xTinggi = anggota.getTinggi_badan();
+                String xBerat = anggota.getBerat_badan();
+                String xNoWa = anggota.getNomor_whatsapp();
 
                 Intent kirim = new Intent(ctx, Detail.class);
                 kirim.putExtra("xFoto", xFoto);
@@ -76,6 +79,101 @@ public class Grid extends RecyclerView.Adapter<Grid.ClassViewHolder> {
                 ctx.startActivity(kirim);
             }
         });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                pesan.setTitle("Perhatian");
+                pesan.setMessage("Anda memilih anggota dengan nama " + anggota.getNama());
+                pesan.setCancelable(true);
+
+                pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteAnggota(anggota.getId());
+                    }
+                });
+
+                pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent kirim = new Intent(ctx, UpdateAnggota.class);
+                        kirim.putExtra("xId", anggota.getId());
+                        kirim.putExtra("xFoto", anggota.getFoto());
+                        kirim.putExtra("xEmail", anggota.getEmail());
+                        kirim.putExtra("xNama", anggota.getNama());
+                        kirim.putExtra("xNpm", anggota.getNpm());
+                        kirim.putExtra("xProdi", anggota.getProdi());
+                        kirim.putExtra("xSabuk", anggota.getSabuk());
+                        kirim.putExtra("xTempat", anggota.getTempat_lahir());
+                        kirim.putExtra("xTanggal", anggota.getTanggal_lahir());
+                        kirim.putExtra("xTinggi", anggota.getTinggi_badan());
+                        kirim.putExtra("xBerat", anggota.getBerat_badan());
+                        kirim.putExtra("xNoWa", anggota.getNomor_whatsapp());
+
+                        ctx.startActivity(kirim);
+                    }
+                });
+
+                pesan.show();
+                return true; // Mengembalikan true agar event onLongClick tidak disalurkan ke event onClick
+            }
+        });
+    }
+
+    private void deleteAnggota(String id) {
+        APIService api = Utilities.getRetrofit().create(APIService.class);
+        Call<ValueNoData> call = api.deleteAnggota(id);
+        call.enqueue(new Callback<ValueNoData>() {
+            @Override
+            public void onResponse(Call<ValueNoData> call, Response<ValueNoData> response) {
+                if (response.isSuccessful()) {
+                    ValueNoData value = response.body();
+                    if (value != null && value.getSuccess() == 1) {
+                        Toast.makeText(ctx, value.getMessage(), Toast.LENGTH_SHORT).show();
+                        getAllAnggota();
+                    } else {
+                        Toast.makeText(ctx, "Failed to delete data: " + value.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ctx, "Response failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ValueNoData> call, Throwable t) {
+                Toast.makeText(ctx, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getAllAnggota() {
+        APIService apiService = Utilities.getRetrofit().create(APIService.class);
+        Call<ValueData<List<AnggotaModel>>> call = apiService.getAnggota();
+        call.enqueue(new Callback<ValueData<List<AnggotaModel>>>() {
+            @Override
+            public void onResponse(Call<ValueData<List<AnggotaModel>>> call, Response<ValueData<List<AnggotaModel>>> response) {
+                if (response.isSuccessful()) {
+                    ValueData<List<AnggotaModel>> value = response.body();
+                    if (value != null && value.getSuccess() == 1) {
+                        dataAnggota.clear();
+                        dataAnggota.addAll(value.getData());
+                        gridAdapter.notifyDataSetChanged();
+                        Toast.makeText(ctx, value.getMessage(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(ctx, "Failed to get data: " + value.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ctx, "Response failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ValueData<List<AnggotaModel>>> call, Throwable t) {
+                Toast.makeText(ctx, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -85,6 +183,14 @@ public class Grid extends RecyclerView.Adapter<Grid.ClassViewHolder> {
 
     public void setData(ArrayList<AnggotaModel> data) {
         this.dataAnggota = data;
+    }
+
+    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+        this.onItemLongClickListener = onItemLongClickListener;
+    }
+
+    public interface OnItemLongClickListener {
+        void onLongClick(AnggotaModel anggota);
     }
 
     public class ClassViewHolder extends RecyclerView.ViewHolder {
